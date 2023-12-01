67289bb1154ab049a7b68bbd2d468b80361ed536
HuertaV <112195133+HuertaV@users.noreply.github.com>
2023-12-01 11:12:22 -0800

    addsasda

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Slider slider;
+    public GameObject BossHealth;
+
+    public void SetHealth(int, 1)
+    {
+        slider.value = 1;
+    }
+
+}
