00901ade44f497a4e6e4dae77baf6d01613be269
OkropiridzeReziko <142932058+OkropiridzeReziko@users.noreply.github.com>
2023-12-12 14:24:49 -0800

    Button Changes, Instruction Changes, Timer added on level 3rd

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 20f;
+     public UIManager manager;
+
+    [SerializeField] Text countdownText;
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("F0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            countdownText.text = currentTime.ToString("F0");
+            manager.GoToPage(2);
+        }
+    }
+}
